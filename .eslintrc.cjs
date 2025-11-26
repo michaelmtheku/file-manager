216602802fad3ad73a23a1df968608773154ef7b
michaelmtheku <sandilethabethe56@gmail.com>
2025-11-26 10:16:31 +0200

    chore: add lint/format scripts and Husky pre-commit hook

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: { es2021: true },
+  parserOptions: { ecmaVersion: 2021, sourceType: 'module' },
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'prettier'],
+  plugins: ['import', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  },
+  overrides: [
+    {
+      files: ['backend/**/*.js', 'backend/**/*.mjs'],
+      env: { node: true },
+      parserOptions: { ecmaVersion: 2021, sourceType: 'module' }
+    },
+    {
+      files: ['frontend/**/*.{js,jsx}'],
+      env: { browser: true, es2021: true },
+      parser: '@babel/eslint-parser',
+      parserOptions: { ecmaFeatures: { jsx: true }, requireConfigFile: false },
+      plugins: ['react', 'react-hooks', 'prettier'],
+      extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+      settings: { react: { version: 'detect' } },
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'prettier/prettier': 'error'
+      }
+    }
+  ]
+};
